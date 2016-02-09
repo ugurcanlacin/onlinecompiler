f977e22ae418085796eac049b5a088eca0e0e5a6
Uğurcan Laçin <lacinugur@hotmail.com.tr>
2016-02-10 00:37:56 +0200

    First commit.

---
+++ src/main/java/com/onlinecompiler/OnlinecompilerApplication.java
@@ -0,0 +1,12 @@
+package com.onlinecompiler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinecompilerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinecompilerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/onlinecompiler/OnlinecompilerApplicationTests.java
@@ -0,0 +1,18 @@
+package com.onlinecompiler;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = OnlinecompilerApplication.class)
+@WebAppConfiguration
+public class OnlinecompilerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
