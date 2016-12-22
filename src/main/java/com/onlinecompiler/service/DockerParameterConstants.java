450c93f5680b8ab8d20e8faa60449b5e32fc5207
Uğurcan Laçin <ugurcanlacin@gmail.com>
2016-12-22 16:54:24 +0300

    Updated project path definition.
    It is more dynamic right now

--- src/main/java/com/onlinecompiler/service/DockerParameterConstants.java
+++ src/main/java/com/onlinecompiler/service/DockerParameterConstants.java
@@ -2,7 +2,7 @@ package com.onlinecompiler.service;
 
 public class DockerParameterConstants {
 	public static final String folder = "temp/";
-	public static final String path = System.getProperty("user.home") + "/git/onlinecompiler/dockerinstallation/";
+	public static final String path = System.getProperty("user.dir") + "/dockerinstallation/";
 	public static final String virtualMachine = "virtual_machine";
 	public static final String timeoutSecond = "20";
 }
